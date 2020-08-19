07f80b6304bef2eb57241200ef9f0b44b0f78c46
natarazworld <natarazworld@gmail.com>
2020-08-19 19:43:57 +0530

    ORM-CURD operations

--- ORMProj1-SpringWithHB-Xml-Annoations/src/main/java/com/nt/service/ProjectMgmtService.java
+++ ORMProj1-SpringWithHB-Xml-Annoations/src/main/java/com/nt/service/ProjectMgmtService.java
@@ -1,9 +1,15 @@
 package com.nt.service;
 
+import java.util.List;
+
 import com.nt.dto.ProjectDTO;
 
 public interface ProjectMgmtService {
 	
 	public  String registerProject(ProjectDTO  dto);
+	public  Object  fetchProjectById(int id);
+	public   String  modifyProjectById(int id, int teamSize, double cost);
+	public  String   removeProjectById(int id);
+	public  List<ProjectDTO>  fetchProjectsByCostRange(double start, double end);
 
 }
--- ORMProj1-SpringWithHB-Xml-Annoations/src/main/java/com/nt/service/ProjectMgmtServiceImpl.java
+++ ORMProj1-SpringWithHB-Xml-Annoations/src/main/java/com/nt/service/ProjectMgmtServiceImpl.java
@@ -1,5 +1,8 @@
 package com.nt.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,6 +28,56 @@ public class ProjectMgmtServiceImpl implements ProjectMgmtService {
 		  //use DAO
 		idVal=dao.insert(entity);
 		return "Project is registered with the id ::"+idVal;
+	}
+
+	@Override
+	public  Object fetchProjectById(int id) {
+		Project proj=null;
+		ProjectDTO dto=null;
+		//use DAO
+		proj=dao.getProjectById(id);
+		//convert to entity object DTO
+		 
+		 if(proj!=null) {
+			 dto=new ProjectDTO();
+		     BeanUtils.copyProperties(proj, dto);
+		 }
+		return dto!=null?dto:"record not found";
+          
+	}
+
+	@Override
+	public String modifyProjectById(int id, int teamSize, double cost) {
+		boolean flag=false;
+		//use DAO
+		flag=dao.updateProjectById(id, teamSize, cost);
+		return flag==false?"record not found for update": "record updated";
+	}
+
+	@Override
+	public String removeProjectById(int id) {
+		   boolean flag=false;
+	        //use DAO
+		   flag=dao.deleteProjectById(id);
+		   
+		return flag==true?"record found and deleted":"record not found for deletion";
+	}
+
+	@Override
+	public List<ProjectDTO> fetchProjectsByCostRange(double start, double end) {
+		List<Project> listEntities=null;
+		List<ProjectDTO> listDTO=new ArrayList();
+		
+		//use DAO
+		listEntities=dao.getProjectsByCostRange(start, end);
+		//convert  listEntties to listDTOs
+		listEntities.forEach(entity->{
+		   	ProjectDTO dto=new ProjectDTO();
+		   	BeanUtils.copyProperties(entity, dto);
+		   	listDTO.add(dto);
+		});
+		
+		return listDTO;
 	}
 
 }
