8d8bd6ca170e6477cb0910962d6ccb7815a5862a
natarazworld <natarazworld@gmail.com>
2020-09-02 19:50:32 +0530

    @Query methods

--- SpringDataProj6-JpaRepo-@QueryMethods/src/main/java/com/nt/SpringDataProj6QueryMethods.java
+++ SpringDataProj6-JpaRepo-@QueryMethods/src/main/java/com/nt/SpringDataProj6QueryMethods.java
@@ -3,16 +3,19 @@ package com.nt;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.nt.repo.CustomerRepo;
 
 @SpringBootApplication
 public class SpringDataProj6QueryMethods {
-
+	
+	
 	public static void main(String[] args) {
 		 ApplicationContext ctx=null;
 		 CustomerRepo custRepo=null;
@@ -25,7 +28,7 @@ public class SpringDataProj6QueryMethods {
 		//custRepo.getCustomersByCity("hyd").forEach(System.out::println);
 		//custRepo.getCustomersByBillAmtRange(8000,4000).forEach(System.out::println);
 		//custRepo.getCustomersByCityNames("hyd","delhi", "vizag").forEach(System.out::println);
-		//custRepo.getCustomerByName("raja").forEach(System.out::println);
+		// custRepo.getCustomerByName("raja").forEach(System.out::println);
 		//custRepo.getCustomersByCnoRange(1, 3).forEach(System.out::println);
 		/*custRepo.getDataValuesByCity("hyd").forEach(row->{
 			//System.out.println(row[0]+"  "+row[1]);
@@ -35,7 +38,26 @@ public class SpringDataProj6QueryMethods {
 			System.out.println();
 		});*/
 		
-		custRepo.getBillAmtByCities("hyd", "delhi").forEach(System.out::println);
+		//custRepo.getBillAmtByCities("hyd", "delhi").forEach(System.out::println);
+		//System.out.println(custRepo.getCustomerByCname("raja"));
+		/*Object data=custRepo.getDataValuesByCname("raja");
+		 Object data1[]=(Object[])data;
+		System.out.println(data1[0]+"  "+data1[1]); */
+		
+		//System.out.println("max bill Amount::"+custRepo.findMaxBillAmount());
+		Object results[]=(Object[]) custRepo.findAggregateResults();
+		System.out.println("max ::"+results[0]);
+		System.out.println("sum ::"+results[1]);
+		System.out.println("avg ::"+results[2]);
+		System.out.println("count ::"+results[3]);
+		
+		 
+	/*	   double bAmt=custRepo.getBillAmtByCname("raja");
+				   System.out.println("Bill Amount::"+bAmt);*/
+		//--------------------non select operations ----------------------
+		// System.out.println("no.of records updated::"+custRepo.modifyCustomerBillAmtByCity("hyd",200));
+		 //System.out.println("no.of records deleted::"+custRepo.deleteCustomersIfCaddIsNull());
+		  
 		
 		   //close container
 		   ((ConfigurableApplicationContext) ctx).close();
--- SpringDataProj6-JpaRepo-@QueryMethods/src/main/java/com/nt/repo/CustomerRepo.java
+++ SpringDataProj6-JpaRepo-@QueryMethods/src/main/java/com/nt/repo/CustomerRepo.java
@@ -2,10 +2,14 @@ package com.nt.repo;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.nt.entity.Customer;
+
+@Transactional
 public interface CustomerRepo extends  JpaRepository<Customer,Integer> {
      //-------- select bulk operations with Postional params (Entity queries (all col values) -----------------------------------
 	   @Query("FROM  Customer")
@@ -24,7 +28,7 @@ public interface CustomerRepo extends  JpaRepository<Customer,Integer> {
     		                                                                             @Param("cityThree")String city3);
 	   
 	   @Query("FROM Customer  cust WHERE cust.cname=:name")
-	   Iterable<Customer>  getCustomerByName(@Param("name")String name );
+	   Iterable<Customer>  getCustomerByName(String name );
 	   
 	   /*@Query("FROM Customer  WHERE cno>=?1 and cno<=:max")    //invalid query
 	   Iterable<Customer>  getCustomersByCnoRange(Integer min ,@Param("max")Integer end); */
@@ -37,6 +41,39 @@ public interface CustomerRepo extends  JpaRepository<Customer,Integer> {
 		   @Query("SELECT billAmt FROM Customer WHERE cadd IN(:city1,:city2)")
 		   Iterable<Double>  getBillAmtByCities(@Param("city1")String city1,
 				                                                         @Param("city2")String city2);
+		   
+	   //--------------------------------------SingleRow select operation using HQL/JPQL  (Entity query) -------------
+		   @Query("FROM Customer WHERE  cname=:name")  //assume "CNAME" col is having unique
+		   Customer   getCustomerByCname(String name);
+		 //--------------------------------------SingleRow select operation using HQL/JPQL  (Scalar query-selecting multiple specific cols) -------------
+		   
+		   @Query("SELECT cno,cname FROM Customer WHERE  cname=:name")
+		 Object  getDataValuesByCname(String name);
+		   
+		 //--------------------------------------SingleRow select operation using HQL/JPQL  (Scalar query-selecting single specific col) -------------
+		   @Query("SELECT billAmt FROM Customer WHERE  cname=:name")
+		   Double  getBillAmtByCname(String name);
+		   
+		   //  ----------------------------- select query aggregate functions ------------------------------
+		    @Query("SELECT MAX(billAmt) FROM Customer")
+		     Double    findMaxBillAmount();
+		    
+		    @Query("SELECT MAX(billAmt),SUM(billAmt),AVG(billAmt),COUNT(*) FROM Customer")
+		     Object    findAggregateResults();
+		    
+		   
+		   //-----------------------------update opeartion  --------------------------------
+		   @Modifying
+		   @Query("UPDATE Customer SET  billAmt=billAmt+ :extraAmount WHERE cadd=:city")
+		   int  modifyCustomerBillAmtByCity(String city ,double extraAmount);
+		   
+		   //-----------------------------delete opeartion  --------------------------------
+		   @Modifying
+		   @Query("DELETE FROM Customer WHERE cadd IS NULL")
+		   int  deleteCustomersIfCaddIsNull();
+
+		   
+		   
 		   
 		    
 	   
