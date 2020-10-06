fe094b7d467565b29816a6c1c5e9c35fbcb48d78
natarazworld <natarazworld@gmail.com>
2020-10-06 19:50:52 +0530

    spring mvc

---
+++ MVCPRoj12-LoginApp-LayeredApp/src/main/java/com/nt/bo/UserBO.java
@@ -0,0 +1,19 @@
+package com.nt.bo;
+
+public class UserBO {
+   private String uname;
+   private String pwd;
+public String getUname() {
+	return uname;
+}
+public void setUname(String uname) {
+	this.uname = uname;
+}
+public String getPwd() {
+	return pwd;
+}
+public void setPwd(String pwd) {
+	this.pwd = pwd;
+}
+   
+}
---
+++ MVCPRoj12-LoginApp-LayeredApp/src/main/java/com/nt/model/User.java
@@ -0,0 +1,21 @@
+package com.nt.model;
+
+import java.io.Serializable;
+
+public class User  {
+   private String uname;
+   private String pwd;
+public String getUname() {
+	return uname;
+}
+public void setUname(String uname) {
+	this.uname = uname;
+}
+public String getPwd() {
+	return pwd;
+}
+public void setPwd(String pwd) {
+	this.pwd = pwd;
+}
+   
+}
